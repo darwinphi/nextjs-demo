c37606eb58aae0878246dbacc7f004c199e2b7a5
Darwin Manalo <darwinmanalophi@gmail.com>
2022-04-18 15:02:38 +0800

    Add ssg, Fetch from internal file

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,28 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Hello from "./hello";
 
-export default function Home() {
+import fs from "fs/promises";
+import path from "path";
+
+export default function Home({ products }) {
   return (
     <div>
       <Hello name="Darwin" />
+      {products.map((prop) => {
+        return <p key={prop.id}>{prop.title}</p>;
+      })}
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      products: data.products,
+    },
+  };
 }
