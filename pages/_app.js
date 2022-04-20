f0c0b72d882459f7e7e50e30e8f4c43d119af71f
Darwin Manalo <darwinmanalophi@gmail.com>
2022-04-20 13:14:29 +0800

    Merging Head

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>General Title</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />;
