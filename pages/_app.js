2b40ff433e27760b715c67feae41a0fe73d4074b
Darwin Manalo <darwinmanalophi@gmail.com>
2022-04-21 10:50:37 +0800

    Remove crap

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
         <title>General Title</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
